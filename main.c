635d9626991b7000f18565597cd09b245c2fc25c
Clamore <rotuman.lenoob@gmail.com>
2020-12-28 11:29:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include "definition.h"
 #include "base_connaissance.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    printf("Entrez le numéro correspondant à l'action que vous souhaitez effectuer\n")
+    int menu;
+    printf("Entrez le numéro correspondant à l'action que vous souhaitez effectuer\n\n");
+    printf("1 - accéder à la base de connaissance\n\n");
+    printf("2 - Entrer une base de faits" );
+    printf("3 - quitter\n\n");
+    scanf("%d",&menu);
+    switch (menu){
+    case 1:
+        afficheBC();//////////////////TODO
+        break;
+    case 2:
+        createBF();///////////////////TODO
+        break;
+    default:
+        break;
+    }
 }
