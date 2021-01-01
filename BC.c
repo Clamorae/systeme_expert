eb777089c506b2408301d94cb8534df88c963496
Clamore <rotuman.lenoob@gmail.com>
2021-01-01 20:52:52 +0100

    deleterule

--- BC.c
+++ BC.c
@@ -15,6 +15,28 @@ void afficheBC(KB BC){
     }
 }
 
+void deleteRule(KB BC,Proposition conc){
+    if (BC==NULL){
+        printf("La base de connaissance est vide\n\n");
+    }else{
+        KB save;
+        while (BC->head->conclusion!=conc||BC->head!=NULL){
+            save=BC;
+            BC=BC->next;
+        }
+        Premisse delete;
+        while (BC->head->premisse!=NULL){
+            delete=BC->head->premisse;
+            BC->head->premisse=BC->head->premisse->next;
+            free(delete);
+        }
+        free(BC->head->conclusion);
+        free(BC->head);
+        save->next=BC->next;
+        free(BC);
+    }
+}
+
 void addruletoBC(KB base){
    Regle* rule=createRule();
    if (base->head==NULL){
