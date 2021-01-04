26448982c3f3f6c95c92935c7c63104061b32177
Clamore <rotuman.lenoob@gmail.com>
2021-01-04 10:42:52 +0100

    again

--- BC.h
+++ BC.h
@@ -8,6 +8,12 @@
 #include <stdio.h>
 
 
+KB createbasis();
+// fonction qui va créer une base de connaissance et renvoyer le pointeur vers cette base
+
+Regle* firstRule(KB base);
+// focntion renvoyant la premiere règle d'une base de connaissance(prise en entrée)
+
 void afficheBC(KB BC);
 // la fonction va parcourir toute la prémisse de la base de connaissance fourni en entrée pour écrire chacuen des propositions puis écrire une flchez et la conclusion
 
