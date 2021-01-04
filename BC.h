e8d0e6853f38a7bbb3a6adfcb5b094780f73465d
Clamore <rotuman.lenoob@gmail.com>
2021-01-04 10:39:27 +0100

    we did it boy base_connaissance is no more

--- BC.h
+++ BC.h
@@ -3,10 +3,16 @@
 
 #include "main.h"
 #include "base_connaissance.h"
+#include "definition.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+KB createbasis();
+// fonction qui va créer une base de connaissance et renvoyer le pointeur vers cette base
+
+Regle* firstRule(KB base);
+// fonction renvoyant la premiere règle d'une base de connaissance(prise en entrée)
 
 void afficheBC(KB BC);
 // la fonction va parcourir toute la prémisse de la base de connaissance fourni en entrée pour écrire chacuen des propositions puis écrire une flchez et la conclusion
